5e46264de8c3d3b821de43dd87cb23284989df4d
Jean <jean.carlosmolossi@gmail.com>
2022-06-16 14:39:30 -0300

    docs: added hook documentation

--- src/presentation/hooks/use-child-counter.ts
+++ src/presentation/hooks/use-child-counter.ts
@@ -1,4 +1,15 @@
-export function useChildCounter() {
+/**
+ * useChildCounter hook
+ *
+ * Example:
+ * ```
+ *      const { next } = useChildCounter();
+ *      <Component child={next()} /> // child will be 1
+ *      <Component child={next()} /> // child will be 2
+ * ```
+ * @returns A callback function to increment child number and return it
+ */
+export function useChildCounter(): () => number {
     let counter = 1;
 
     return () => ++counter;
