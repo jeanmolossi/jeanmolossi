9c8c4b49c00910ce27743e0af226774c07d9c6b5
Jean <jean.carlosmolossi@gmail.com>
2022-06-17 00:45:04 -0300

    fix: load p5 wrapper after screen loads

--- src/presentation/components/_layout/layout/index.tsx
+++ src/presentation/components/_layout/layout/index.tsx
@@ -1,19 +1,19 @@
-import { ReactNode } from 'react'
-import dynamic from 'next/dynamic';
+import React, { ReactNode, Suspense } from 'react'
+import Canvas from '@/presentation/components/canvas';
 import { Navbar } from '../navbar';
 import { MainContainer } from './styles'
 
-const Canvas = dynamic(() => import('@/presentation/components/canvas'), { ssr: false })
 
 interface LayoutProps {
     children?: ReactNode;
 }
 
 export function Layout({ children }: LayoutProps) {
-  
     return (
         <>
-            <Canvas />
+            <Suspense fallback="Loading...">
+                <Canvas />
+            </Suspense>
             <MainContainer>
                 {children}
 
--- src/presentation/components/canvas/index.tsx
+++ src/presentation/components/canvas/index.tsx
@@ -1,12 +1,20 @@
-import { ReactP5Wrapper } from 'react-p5-wrapper'
+import { useEffect, useState } from 'react';
 import { sketch } from './sketches/hex';
 import { CanvasOverlay } from './styles';
 
 export default function Canvas() {
+    const [Wrapper, setWrapper] = useState<any>(null);
+
+    useEffect(() => {
+        if (typeof window === 'undefined') return;
+        const { ReactP5Wrapper } = require('react-p5-wrapper');
+
+        setWrapper(<ReactP5Wrapper sketch={sketch} />)
+    }, [])
+
     return (
         <CanvasOverlay>
-            <ReactP5Wrapper sketch={sketch} />
+            {Wrapper}
         </CanvasOverlay>
     )
 }
-
