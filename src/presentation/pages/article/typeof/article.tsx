6d2c8d317bdf70026aa4c4fd0adbce5f2f8875b8
Jean <jean.carlosmolossi@gmail.com>
2022-06-18 13:52:36 -0300

    chore using lazy load

--- src/presentation/pages/article/typeof/article.tsx
+++ src/presentation/pages/article/typeof/article.tsx
@@ -1,7 +1,15 @@
-import { Markdown } from "@/presentation/components"
+import { FunctionComponent } from "react"
+import dynamic from "next/dynamic"
+import { MarkdownProps } from "@/presentation/components"
 import { TagList } from "../tag-list"
 import { ArticleProps } from ".."
 import * as S from "../styles"
+
+const LazyMd = dynamic(
+    () => import("../../../components/_layout/markdown")
+        .then((mod) => mod.Markdown),
+    { loading: () => <div>Carregando...</div> },
+) as FunctionComponent<MarkdownProps>
 
 export const TheArticle = ({ article }: ArticleProps) => {
     return (
@@ -9,9 +17,9 @@ export const TheArticle = ({ article }: ArticleProps) => {
             <h1>{ article.title }</h1>
             <h2>{ article.description }</h2>
 
-            <Markdown>
+            <LazyMd>
                 { article.body_markdown }
-            </Markdown>
+            </LazyMd>
 
             <TagList tagList={article.tag_list} />
         </S.Article>
