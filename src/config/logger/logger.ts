1d5d740e0aa2b658b5009daeb06d9e53f4cae469
Jean <jean.carlosmolossi@gmail.com>
2022-06-18 14:42:14 -0300

    chore: added pino logger

---
+++ src/config/logger/logger.ts
@@ -0,0 +1,26 @@
+import os from 'os';
+import pino from 'pino';
+import { randomUUID } from 'crypto';
+
+const logger = pino(
+    {
+        base: {
+            env: process.env.NODE_ENV,
+            pid: process.pid,
+            hostname: os.hostname(),
+        },
+        level: 'info',
+        messageKey: 'message',
+        formatters: {
+            level: level => ({ level }),
+            log: options => ({
+                ...options,
+                'transaction-id': randomUUID(),
+            }),
+        },
+        timestamp: () => `,"time":"${new Date(Date.now()).toISOString()}"`,
+    },
+    process.stdout,
+);
+
+export default logger;
