ca38fdc5fc77b7a93a88c61af78bf370ed901cc5
Jean <jean.carlosmolossi@gmail.com>
2022-06-18 18:04:44 -0300

    chore: added image domain resources

--- next.config.js
+++ next.config.js
@@ -3,7 +3,13 @@ const nextConfig = {
     reactStrictMode: true,
     compiler: {
         styledComponents: true,
-    }
+    },
+    images: {
+        domains: [
+            'res.cloudinary.com',
+            's3.amazonaws.com',
+        ]
+    },
 }
 
 module.exports = nextConfig
