3394f4e07da23d683aedbe97f32252b683c7c385
Jean <jean.carlosmolossi@gmail.com>
2022-06-18 16:27:01 -0300

    feat: added revalidate property

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -14,6 +14,25 @@ export const getStaticProps: GetStaticProps<BlogProps> = async () => {
     return {
         props: {
             articles
-        }
+        },
+        revalidate: timeIn('01d')
     }
+}
+
+type AcceptNum = 0|1|2|3|4|5|6|7|8|9;
+type Timer = `${AcceptNum}${AcceptNum}${"s" | "m" | "h" | "d"}`;
+function timeIn(t: Timer): number {
+    const timeMap = {
+        s: 1,
+        m: 60,
+        h: 60 * 60,
+        d: 60 * 60 * 24
+    }
+
+    const range = t.replace(/\d/gi,'') as keyof typeof timeMap // parse key of time range
+    const time = +t.replace(/\D/gi,'') // remove letters and convert to number
+
+    const timeRange = timeMap[range] || timeMap.h // select property time range: ;
+
+    return timeRange * time
 }
