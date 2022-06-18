a008cecc1149f121018184d58f5b5ab8cfef78c1
Jean <jean.carlosmolossi@gmail.com>
2022-06-18 16:05:44 -0300

    infra: added env

--- terraform/amplify/local.tf
+++ terraform/amplify/local.tf
@@ -11,11 +11,12 @@ locals {
       sub_domain  = format("%s", var.route53_zone_name)
       branch_name = "main"
 
-      env                         = ""
-      DEV_TO_BASE_URL             = ""
-      DEV_TO_API_KEY              = ""
-      NEXT_PUBLIC_DEV_TO_BASE_URL = ""
-      NEXT_PUBLIC_GA_TRACKING_ID  = ""
+      env                          = ""
+      DEV_TO_BASE_URL              = ""
+      DEV_TO_API_KEY               = ""
+      NEXT_PUBLIC_DEV_TO_BASE_URL  = ""
+      NEXT_PUBLIC_GA_TRACKING_ID   = ""
+      NEXT_PUBLIC_GTM_TRACKING_TAG = ""
     }
   }
 }
