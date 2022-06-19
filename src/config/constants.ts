37fd5a3228bc8b4b0ea85f600aadc85d96e746cf
Jean <jean.carlosmolossi@gmail.com>
2022-06-19 01:16:03 -0300

    feat: added social links

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -2,3 +2,12 @@ export const gtag = {
     GA_TRACKING_ID: process.env.NEXT_PUBLIC_GA_TRACKING_ID,
     GTM_TRACKING_TAG: process.env.NEXT_PUBLIC_GTM_TRACKING_TAG,
 };
+
+export const social = {
+    github: 'https://github.com/jeanmolossi',
+    twitter: 'https://twitter.com/jeanmolossi',
+    linkedin: 'https://www.linkedin.com/in/jean-carlos-molossi-512098126/',
+    youtube: 'https://www.youtube.com/channel/UCWQyi_jJN_C-yVffPleNlaQ',
+    telegram: 'https://t.me/jeanmolossi',
+    instagram: 'https://instagram.com/jeancarlosmolossi',
+} as const;
