bc3ee1db2c95070f9361bf1f7673fd37527fd1e4
Jean <jean.carlosmolossi@gmail.com>
2022-06-19 01:17:39 -0300

    feat: pick social colors

--- src/presentation/styles/global.ts
+++ src/presentation/styles/global.ts
@@ -80,3 +80,12 @@ export const theme = {
 };
 
 export type Theme = typeof theme;
+
+export const socialColors = {
+    github: '#000000',
+    twitter: '#348bc4',
+    linkedin: '#0b66c3',
+    youtube: '#fe0001',
+    telegram: '#2da5df',
+    instagram: '#d73161',
+} as const;
