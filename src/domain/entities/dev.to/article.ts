8e35a7a8b4462980259d3854c35941d41a3a59ee
Jean <jean.carlosmolossi@gmail.com>
2022-06-19 01:15:49 -0300

    refactor: split articles definitions

--- src/domain/entities/dev.to/article.ts
+++ src/domain/entities/dev.to/article.ts
@@ -5,24 +5,19 @@ export enum TypeofArticle {
 /**
  * @param {string} tag_list - Comma separated list of tags (e.g. "tag1, tag2, tag3")
  */
-export interface Article {
+interface BaseArticle {
     type_of: TypeofArticle;
     id: number;
     title: string;
     description: string;
     cover_image: string;
-    published: true;
     published_at: string;
     // comma separated list of tags
     tag_list: string;
     slug: string;
-    path: string;
-    url: string;
-    canonical_url: string;
     comments_count: number;
     positive_reactions_count: number;
     public_reactions_count: number;
-    page_views_count: number;
     published_timestamp: string;
     body_markdown: string;
     user: {
@@ -35,16 +30,10 @@ export interface Article {
         profile_image_90: string;
     };
     reading_time_minutes: number;
-    organization: {
-        name: string;
-        username: string;
-        slug: string;
-        profile_image: string;
-        profile_image_90: string;
-    };
-    flare_tag: {
-        name: string;
-        bg_color_hex: string;
-        text_color_hex: string;
-    };
 }
+
+export interface ListingArticle extends BaseArticle {
+    page_views_count: number;
+}
+
+export interface Article extends BaseArticle {}
