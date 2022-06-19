c6dbdef54a9dad8aa56afe676676da9cf7bd6f4e
Jean <jean.carlosmolossi@gmail.com>
2022-06-19 18:44:14 -0300

    feat: created use media hook

--- src/presentation/hooks/index.ts
+++ src/presentation/hooks/index.ts
@@ -1,3 +1,4 @@
 export * from './use-child-counter';
+export * from './use-media-query';
 export * from './use-modal';
 export * from './use-on-screen';
---
+++ src/presentation/hooks/use-media-query.ts
@@ -0,0 +1,14 @@
+import { useEffect, useState } from 'react';
+
+export function useMediaQuery(query: string) {
+    const [mediaQuery, setMediaQuery] = useState(false);
+
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            const mql = window.matchMedia(query);
+            setMediaQuery(mql.matches);
+        }
+    }, [query]);
+
+    return mediaQuery;
+}
