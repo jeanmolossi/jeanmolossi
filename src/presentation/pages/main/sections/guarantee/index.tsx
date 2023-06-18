83f37d002e7773be9659e00551244d8e0f71de78
Jean Molossi <jean.carlosmolossi@gmail.com>
2023-06-18 02:56:08 -0300

    feat: created other sections

--- src/presentation/pages/main/sections/guarantee/index.tsx
+++ src/presentation/pages/main/sections/guarantee/index.tsx
@@ -1,3 +1,15 @@
 export function Guarantee() {
-    return <>Guarantee</>
+    return (
+        <section className="bg-neutral-800 w-full py-8 px-4">
+            <div className="max-w-7xl mx-auto grid grid-cols-1 gap-8">
+                <h2>Garantia de satisfação</h2>
+
+                <p>
+                    Estou tão confiante de que você encontrará valor em nosso e-book{' '}
+                    que ofereço uma garantia de satisfação de 30 dias. Se você não{' '}
+                    ficar satisfeito com o conteúdo, reembolsaremos seu dinheiro sem questionamentos.
+                </p>
+            </div>
+        </section>
+    )
 }
--- src/presentation/pages/main/sections/last-call-to-action/index.tsx
+++ src/presentation/pages/main/sections/last-call-to-action/index.tsx
@@ -1,3 +1,14 @@
 export function LastCallToAction() {
-    return <>Buy Now</>
+    return (
+        <section className="bg-neutral-800 w-full py-8 px-4">
+            <div className="max-w-7xl mx-auto grid grid-cols-1 lg:grid-cols-[1fr,_clamp(20rem,100%,34rem)] gap-8">
+                <p>Não perca esta oportunidade de obter um impulso significativo em sua carreira como desenvolvedor. Junte-se a centenas de desenvolvedores iniciantes que já se beneficiaram com as estratégias compartilhadas neste e-book.</p>
+
+                <div>
+                    <p>📚 Faça o download agora mesmo e comece a alavancar sua carreira como desenvolvedor!</p>
+                    <a href="#">Garantir e-book</a>
+                </div>
+            </div>
+        </section>
+    )
 }
