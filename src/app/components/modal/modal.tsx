690c5455c2940de0495416465b1a9910d8c51682
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2023-10-13 22:03:53 -0300

    feat: add some movement to modal

--- src/app/components/modal/modal.tsx
+++ src/app/components/modal/modal.tsx
@@ -3,6 +3,7 @@
 import { useRouter } from "next/navigation";
 import React, { MouseEventHandler, useCallback, useEffect, useRef } from "react";
 import { FiX } from "react-icons/fi";
+import styles from './modal.module.css';
 
 export default function Modal({
     children
@@ -54,17 +55,12 @@ export default function Modal({
     return (
         <div
             ref={overlay}
-            className="fixed z-10 left-0 right-0 top-0 h-screen mx-auto bg-black/70 overflow-y-scroll py-8"
+            className={styles.overlay}
             onClick={onClick}
         >
             <div
                 ref={wrapper}
-                className={
-                    'mx-auto ' +
-                    'max-w-3xl w-full p-4 rounded bg-neutral-900 min-h-[16rem] ' +
-                    'z-10 ' +
-                    'grid grid-rows-[2rem_1fr]'
-                }
+                className={styles.wrapper}
             >
                 <div className="grid grid-cols-[1fr_2rem]">
                     <span></span>
