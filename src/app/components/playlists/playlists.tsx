92078d829e46947bce9bf263c32321b4c4cd78b9
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2023-10-14 17:39:10 -0300

    feat: add playlist skeleton

--- src/app/components/playlists/playlists.tsx
+++ src/app/components/playlists/playlists.tsx
@@ -98,7 +98,44 @@ function PlaylistItem({ playlist }: PlaylistItemProps) {
 }
 
 export function PlaylistsSkeleton() {
+    const item = [styles.playlist_item, 'h-[458px]'].join(' ')
+
     return (
-        <div>Loading</div>
+        <div className="animate-pulse w-full">
+            <div className="mt-6 grid grid-cols-1 gap-6">
+                <div className={styles.playlist_list}>
+                    {Array.from({ length: 4 }).map((_, i) => (
+                        <div key={i.toString()} className={item}>
+                            <div className="h-8 w-96 bg-neutral-700 rounded-full"></div>
+
+                            <div className="w-full bg-neutral-700 rounded-md aspect-video flex justify-center items-center">
+                                <svg className="w-10 h-10 text-neutral-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
+                                    <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z"/>
+                                </svg>
+                            </div>
+
+                            <div className="h-3 w-full bg-neutral-700 rounded-full"></div>
+                            <div className="h-3 w-full bg-neutral-700 rounded-full"></div>
+                            <div className="h-3 w-72 bg-neutral-700 rounded-full"></div>
+
+                            <div className="self-end place-self-end h-8 w-48 bg-neutral-700 rounded-full"></div>
+                        </div>
+                    ))}
+                </div>
+
+                <div
+                    aria-hidden={false}
+                    className={styles.divider}
+                ></div>
+
+                <div
+                    aria-hidden={false}
+                    className={styles.pagination}
+                >
+                    <div className="h-8 w-48 bg-neutral-700 rounded-full"></div>
+                    <div className="h-8 w-48 bg-neutral-700 rounded-full"></div>
+                </div>
+            </div>
+        </div>
     )
 }
