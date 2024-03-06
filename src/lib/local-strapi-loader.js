453c3862d98b9ae1a7a44b2e76371b08c37bac66
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2024-03-05 21:13:50 -0300

    fix: external urls

--- src/lib/local-strapi-loader.js
+++ src/lib/local-strapi-loader.js
@@ -3,8 +3,12 @@ export default function strapiImageLoader({ src, width, quality } = {}) {
     const srcWithoutHeadslash = src.replace(/^\//, '');
 
     const isLocalStatic = srcWithoutHeadslash.startsWith('_next');
+    const isStrapiUpload = srcWithoutHeadslash.startsWith('uploads')
+    const isExternal = srcWithoutHeadslash.startsWith('http')
 
-    if (isLocalStatic)
+    if (isExternal) return src;
+
+    if (isLocalStatic || !isStrapiUpload)
         return `http://localhost:3000/${srcWithoutHeadslash}?w=${width}&q=${quality||75}`;
 
     return `http://localhost:1337/${srcWithoutHeadslash}?w=${width}&q=${quality||75}`
