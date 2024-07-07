83fdb9be76f84cf21ce398adf1b6b8f0affdb799
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2024-07-06 23:01:21 -0300

    refactor(navigation): does not use it as slot

--- apps/web/src/app/(portfolio)/layout.tsx
+++ apps/web/src/app/(portfolio)/layout.tsx
@@ -1,4 +1,5 @@
 import { Footer } from "@/presentation/components/_layout/footer";
+import Navigation from "@/presentation/components/_layout/navigation-menu";
 import "@/presentation/helpers";
 import React, { Suspense } from "react";
 
@@ -6,16 +7,14 @@ const HireLogo = React.lazy(() => import('@/presentation/components/_layout/hire
 
 export default function Layout({
     children,
-    navigation,
     modal,
 }: {
     children: React.ReactNode,
-    navigation: React.ReactNode,
     modal: React.ReactNode,
 }) {
     return (
         <div className="app-layout-child mx-auto w-full max-w-screen-2xl bg-background pt-[4.5rem]">
-            {navigation}
+            <Navigation />
 
             {children}
             {modal}
