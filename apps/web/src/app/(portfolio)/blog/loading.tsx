6ecce3ed58b2b335aaec8dd8be26cc669b9eb9dd
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2024-07-08 20:17:57 -0300

    feat(loading): add mobile loading skeleton styles

--- apps/web/src/app/(portfolio)/blog/loading.tsx
+++ apps/web/src/app/(portfolio)/blog/loading.tsx
@@ -4,7 +4,7 @@ import { Skeleton } from "@jeanmolossi/ui";
 export default function Loading() {
     return (
         <div className="my-12 py-6 flex flex-col gap-6">
-            <div className="flex justify-between items-center">
+            <div className="flex flex-col lg:flex-row justify-between lg:items-center gap-4 lg:gap-0 p-8 lg:p-0">
                 <Skeleton className="h-[72px] w-[320px]" />
 
                 <div className="flex flex-col justify-end items-end gap-2">
@@ -13,7 +13,7 @@ export default function Loading() {
                 </div>
             </div>
 
-            <Skeleton className="h-4 w-[280px]" />
+            <Skeleton className="h-4 w-[280px] ml-8 lg:ml-0" />
 
             <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-4">
                 <CardSkeleton />
--- apps/web/src/app/(portfolio)/playlists/loading.tsx
+++ apps/web/src/app/(portfolio)/playlists/loading.tsx
@@ -4,16 +4,17 @@ import { CardSkeleton } from "../_components/card.skeleton";
 export default function Loading() {
     return (
         <div className="my-12 py-6 flex flex-col gap-6">
-            <div className="flex justify-between items-center">
+            <div className="flex flex-col lg:flex-row justify-between lg:items-center gap-4">
                 <Skeleton className="h-[72px] w-[320px]" />
 
-                <div className="flex flex-col justify-end items-end gap-2">
-                    <Skeleton className="h-10 w-[238px]" />
-                    <Skeleton className="h-4 w-[80px]" />
+                <div className="flex flex-col lg:justify-end lg:items-end gap-2">
+                    <Skeleton className="h-4 w-[238px] lg:hidden" />
+                    <Skeleton className="h-10 w-full lg:w-[238px]" />
+                    <Skeleton className="hidden lg:block h-4 w-[80px]" />
                 </div>
             </div>
 
-            <Skeleton className="h-4 w-[280px]" />
+            <Skeleton className="h-4 w-[280px] hidden lg:block" />
 
             <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-4">
                 <CardSkeleton />
