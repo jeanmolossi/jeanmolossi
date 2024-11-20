baeb766b14e264c25f4cba1ce50b9bc3d41fe78f
Jean <jean.carlosmolossi@gmail.com>
2024-11-20 15:51:53 -0300

    refactor: removed deadcode

--- apps/web/src/app/components/video-wrapper/server-video.tsx
+++ apps/web/src/app/components/video-wrapper/server-video.tsx
@@ -1,15 +1,15 @@
 import styles from './video-wrapper.module.css';
 
 export async function VideoComponent({ videoId }: { videoId: string }) {
-    const url = new URLSearchParams()
+    const url = new URLSearchParams();
 
     Object.entries({
-        origin: "https://jeanmolossi.com.br",
+        origin: 'https://jeanmolossi.com.br',
         autoplay: '0',
         start: '0',
-    }).forEach(([key, value]) => url.append(key, value))
+    }).forEach(([key, value]) => url.append(key, value));
 
-    const embedSrcWithConfigs = `https://www.youtube.com/embed/${videoId}?${url.toString()}`
+    const embedSrcWithConfigs = `https://www.youtube.com/embed/${videoId}?${url.toString()}`;
 
     return (
         <div className={styles.video_wrapper}>
@@ -20,5 +20,5 @@ export async function VideoComponent({ videoId }: { videoId: string }) {
                 allowFullScreen
             ></iframe>
         </div>
-    )
+    );
 }
